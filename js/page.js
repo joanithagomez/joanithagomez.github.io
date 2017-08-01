f9e32f509afdf3fd30f0063f6c24f41c4a9fa6fb
joanitagomez@gmail.com <joanitagomez@gmail.com>
2017-08-01 15:03:34 -0700

    Navigation fixed and improved responsiveness

--- js/page.js
+++ js/page.js
@@ -2,19 +2,33 @@ $(document).ready(function() {
 
   $('.title-container').velocity("transition.slideDownBigIn");
 
-  $('.img-container').velocity("transition.slideDownBigIn", {
-    display: "block"
-  });
+  $('.img-container').velocity("transition.slideDownBigIn");
   $('.about-text').velocity("transition.slideDownBigIn", {
     display: "flex"
   });
   var state = "closed";
 
   function closeNavbar() {
-    $('.menu-overlay').velocity('transition.slideRightBigOut');
+
+    $(".menu").velocity("transition.fadeOut", {
+      duration: 300
+    });
+    $(".menu-overlay").velocity({
+      width: "0%"
+    }, {
+      duration: 300
+    });
     state = "closed";
   }
 
+  $(window).on("scroll", function() {
+    if (($(window).scrollTop() > 780) && ($(window).scrollTop() < 2700)) {
+      $(".side-bar").addClass("on-blue");
+    } else {
+      //remove the background property so it comes transparent again (defined in your css)
+      $(".side-bar").removeClass("on-blue");
+    }
+  });
 
   $('.nav-icon').on('click', function(event) {
     event.stopPropagation();
@@ -27,7 +41,18 @@ $(document).ready(function() {
 
     if (state === "closed") {
 
-      $('.menu-overlay').velocity('transition.slideRightBigIn');
+
+      if ($(window).width() <= 768) {
+        $('.menu-overlay').velocity({
+          width: "80%"
+        });
+      } else {
+        $('.menu-overlay').velocity({
+          width: "30%"
+        });
+      }
+      $(".menu").velocity("transition.fadeIn");
+
       $('body').on("click.validator", handle);
       state = "open";
 
