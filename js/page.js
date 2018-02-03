7647bf5998809f558778584d14048d65db6ce83a
joanitagomez@gmail.com <joanitagomez@gmail.com>
2018-02-02 20:32:16 -0800

    Contact page fixed

--- js/page.js
+++ js/page.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
  
-
   $(window).on("scroll", function() {
     if ($(window).scrollTop() > 100) {
       $(".nav-bar").addClass("shrink");
@@ -9,6 +8,44 @@ $(document).ready(function() {
     }
   });
 
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('nav').outerHeight();
 
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 20);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('nav').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('nav').removeClass('nav-up').addClass('nav-down');
+
+        }
+    }
+    
+    lastScrollTop = st;
+}
 
 });
