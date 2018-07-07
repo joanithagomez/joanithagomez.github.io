ae1195998b7bddc516879f70f059cf4a5e4c250c
joanitagomez@gmail.com <joanitagomez@gmail.com>
2018-07-07 13:02:01 -0700

    Updated vault

--- js/page.js
+++ js/page.js
@@ -1,22 +1,24 @@
 $(document).ready(function() {
- 
-  $(window).on("scroll", function() {
-    if ($(window).scrollTop() > 100) {
-      $(".nav-bar").addClass("shrink");
-    } else {
-      $(".nav-bar").removeClass("shrink");
-    }
-  });
+  //
+  // $(window).on("scroll", function() {
+  //   if ($(window).scrollTop() > 100) {
+  //     $(".nav-bar").addClass("shrink");
+  //   } else {
+  //     $(".nav-bar").removeClass("shrink");
+  //   }
+  // });
+  //
+  // $('#menu').on('click', () => {
+  //   if ($('#menu').attr('class') === 'menubtn') {
+  //     $(".menubtn").addClass("responsive");
+  //     console.log(true);
+  //   }
+  //   else {
+  //     console.log($('#menu').attr('class'));
+  //     $(".menubtn").removeClass("responsive");
+  //   }
+  // });
 
-  $('#menu').on('click', () => {
-    if ($('#menu').attr('class') === 'menubtn') {
-      $(".menubtn").addClass("responsive");
-      console.log(true);
-    }
-    else {
-      console.log($('#menu').attr('class'));
-      $(".menubtn").removeClass("responsive");
-    }
-  });
+
 
 });
