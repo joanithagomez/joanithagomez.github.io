a5817ad06769afedaf6dfbb3365412dc56c7b6a1
joanitha <joanitha@securly.com>
2024-05-04 14:51:31 -0700

    Add sticky scroll

--- js/page.js
+++ js/page.js
@@ -8,10 +8,30 @@ $(document).ready(function() {
   //   const darkModeOn = e.matches;
   //   console.log(`Dark mode is ${darkModeOn ? '🌒 on' : '☀️ off'}.`);
   // });
-  
+  stickNav();
   setNumberOfYears();
 });
 
+function stickNav() {
+  let lastScroll = 0;
+  $(window).scroll((event) => {
+    let currentScroll = $(this).scrollTop();
+    if (currentScroll <= 0) {
+      $('.navbar ul').removeClass('box-shadow');
+    } else {
+      $('.navbar ul').addClass('box-shadow');
+    }
+
+    if (currentScroll - lastScroll < 0) {
+      $( ".navbar" ).removeClass('hide');
+      $( ".navbar" ).addClass('slide-down');
+    } else {
+      $( ".navbar" ).addClass('hide');
+      $( ".navbar" ).removeClass('slide-down');
+    }
+    lastScroll = currentScroll;
+  })
+}
 function setNumberOfYears() {
   const startYear = 2019;
   const currentYear = new Date().getFullYear();
