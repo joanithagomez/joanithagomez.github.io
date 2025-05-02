0ad89139608eca9c1ba50dbf629ba70c8df6ce87
joanitha <joanitagomez@gmail.com>
2025-05-02 12:49:38 +0530

    Update portfolio

--- js/page.js
+++ js/page.js
@@ -1,16 +1,46 @@
-$(document).ready(function() {
-  // $('#too-bright').on('click', () => {
-  //  console.log('too bright')
-  // });
+const storageKey = 'theme-preference'
 
-  // const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-  // darkModeMediaQuery.addEventListener('change', (e) => {
-  //   const darkModeOn = e.matches;
-  //   console.log(`Dark mode is ${darkModeOn ? '🌒 on' : '☀️ off'}.`);
-  // });
+$(document).ready(function() {
+  const $circles = $('.circle-item img');
+  const circleEl = $('.skills-circle')[0];
+  const containerSize = Math.min(circleEl.offsetWidth || 800, circleEl.offsetHeight || 800);
+  const radius = (containerSize / 2) * 0.95;
+
+
+  $('#theme-toggle').on('click', function() {
+    switchTheme();
+  });
+
+  // sync with system changes
+  window
+  .matchMedia('(prefers-color-scheme: dark)')
+  .addEventListener('change', ({matches:isDark}) => {
+    theme.value = isDark ? 'dark' : 'light'
+    setPreference();
+  })
+
+  reflectPreference();
   stickNav();
-  setNumberOfYears();
+  circularAnimation($circles, radius);
+  // setNumberOfYears();
 });
+
+
+/**
+ * Positions elements in a circular layout around the center of their container.
+*/
+function circularAnimation($circles, radius) {
+  const numItems = $circles.length;
+  const initialAngleStep = (2 * Math.PI) / numItems;
+  
+  $circles.each(function(i) {
+    const angle = i * initialAngleStep - (Math.PI / 2);
+    const x = radius * Math.cos(angle);
+    const y = radius * Math.sin(angle);
+    $(this).css('left', `calc(50% + ${x}px)`);
+    $(this).css('top', `calc(50% + ${y}px)`);
+  });
+}
 
 function stickNav() {
   let lastScroll = 0;
@@ -32,13 +62,43 @@ function stickNav() {
     lastScroll = currentScroll;
   })
 }
+
+
 function setNumberOfYears() {
   const startYear = 2019;
   const currentYear = new Date().getFullYear();
   $('#numberOfYears').text(currentYear - startYear);
 }
 
-// function switchMode() {
-//   $(document.documentElement).toggleClass('dark');
-//   console.log("switch")
-// }
+
+const theme = {
+  value: getColorPreference(),
+}
+
+function switchTheme() {
+  theme.value = theme.value === 'light'
+    ? 'dark'
+    : 'light';
+  setPreference();
+}
+
+function getColorPreference() {
+  if (localStorage.getItem(storageKey))
+    return localStorage.getItem(storageKey)
+  else
+    return window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light'
+}
+
+function setPreference() {
+  localStorage.setItem(storageKey, theme.value)
+  reflectPreference();
+}
+
+function reflectPreference () {
+  document.firstElementChild
+    .setAttribute('data-theme', theme.value)
+
+  $('#theme-toggle')?.attr('aria-label', theme.value);
+}
